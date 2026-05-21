2e37fea55bf3b43ec75e46f14e2fa21c48f387ff
Paco Eggimann <peggimann@puzzle.ch>
2026-05-21 13:04:11 +0200

    chore(MON-34): bootstrap springboot project

---
+++ core/src/test/java/ch/swisstopo/monteis/core/TestCoreApplication.java
@@ -0,0 +1,11 @@
+package ch.swisstopo.monteis.core;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(CoreApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
