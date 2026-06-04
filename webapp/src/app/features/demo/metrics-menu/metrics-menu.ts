88633e514084bd60afe52efec436910c58240038
Paco Eggimann <peggimann@puzzle.ch>
2026-06-04 21:53:55 +0200

    chore(MON-35): add angular material styling to workbench

--- webapp/src/app/features/demo/metrics-menu/metrics-menu.ts
+++ webapp/src/app/features/demo/metrics-menu/metrics-menu.ts
@@ -1,11 +1,10 @@
-import {Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {Component} from '@angular/core';
 import { RouteButton } from '../../../ui/buttons/route-button/route-button';
 @Component({
   selector: 'app-metrics-menu',
   imports: [RouteButton],
   templateUrl: './metrics-menu.html',
   styleUrl: './metrics-menu.scss',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export default class MetricsMenu {
 }
