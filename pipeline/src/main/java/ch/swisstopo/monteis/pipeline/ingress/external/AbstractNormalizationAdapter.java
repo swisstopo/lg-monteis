96387bb779c956d55ba40446d8149454ccae55e4
urfer <urfer@puzzle.ch>
2026-06-18 18:24:17 +0200

    feat(MON-62): introduces a working pipeline flow and documentation

---
+++ pipeline/src/main/java/ch/swisstopo/monteis/pipeline/ingress/external/AbstractNormalizationAdapter.java
@@ -0,0 +1,31 @@
+package ch.swisstopo.monteis.pipeline.ingress.external;
+
+import ch.swisstopo.monteis.pipeline.ingress.internal.NormalizedSensorData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+
+import java.util.concurrent.CompletableFuture;
+
+public abstract class AbstractNormalizationAdapter {
+
+    private KafkaTemplate<String, NormalizedSensorData> kafkaTemplate;
+
+    private String normalizedTopic;
+
+    @Autowired
+    public void setKafkaTemplate(KafkaTemplate<String, NormalizedSensorData> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @Value("${app.kafka.topics.normalized}")
+    public void setNormalizedTopic(String normalizedTopic) {
+        this.normalizedTopic = normalizedTopic;
+    }
+
+    protected final CompletableFuture<SendResult<String, NormalizedSensorData>> publishNormalizedData(
+            String sensorId, NormalizedSensorData data) {
+        return kafkaTemplate.send(normalizedTopic, sensorId, data);
+    }
+}
