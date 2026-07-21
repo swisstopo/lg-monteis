01a96e592f86692335ab8fe728bfab82fe44a9fd
Miguel Lehmann <lehmann@puzzle.ch>
2026-07-21 16:47:55 +0200

    test(MON-44): test if sonar builds test coverage

---
+++ core/src/main/java/ch/swisstopo/monteis/core/modules/overview/web/OverviewController3.java
@@ -0,0 +1,31 @@
+package ch.swisstopo.monteis.core.modules.overview.web;
+
+import ch.swisstopo.monteis.core.modules.overview.service.OverviewService;
+import ch.swisstopo.monteis.core.modules.overview.web.dto.ReadSimpleMetricDto;
+import jakarta.validation.constraints.Positive;
+import java.util.List;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/overview3")
+public class OverviewController3 {
+
+  private final OverviewService overviewService;
+
+  public OverviewController3(OverviewService overviewService) {
+    this.overviewService = overviewService;
+  }
+
+  @GetMapping(value = "/metrics", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<List<ReadSimpleMetricDto>> getMetrics(
+      @RequestParam(defaultValue = "5") @Positive int limit) {
+
+    List<ReadSimpleMetricDto> result = overviewService.fetchRecentMetrics(limit);
+    return ResponseEntity.ok(result);
+  }
+}
