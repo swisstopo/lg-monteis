0959f5606f115f346c518eb3bd3ac9b70c68a54d
Stephan Girod <girod@puzzle.ch>
2026-07-29 17:27:37 +0200

    feat(MON-57): add translations

--- webapp/src/app/features/sensor/sensor-menu/sensor-menu.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-sensor-menu',
-  imports: [],
-  templateUrl: './sensor-menu.html',
-  styleUrl: './sensor-menu.scss',
-})
-export default class SensorMenu {}
