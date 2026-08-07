e7d1072ab413fa154625d8537a8b46647fde7cb8
Stephan Girod <girod@puzzle.ch>
2026-08-07 12:18:51 +0200

    feat(MON-96): trigger chart from table

--- webapp/src/app/features/sensor/table-header/table-header.ts
+++ webapp/src/app/features/sensor/table-header/table-header.ts
@@ -1,21 +1,33 @@
-import { Component, inject, input, inputBinding } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, inject, input, inputBinding, outputBinding } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatDialog } from '@angular/material/dialog';
 import { MatIcon } from '@angular/material/icon';
 import { MatFormField, MatInput } from '@angular/material/input';
-import { TranslatePipe } from '@ngx-translate/core';
 import { PermissionsService } from '../../../core/auth/permissions.service';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import Chart from '../../../ui/chart/chart';
+import {
+  generateMockHumidityDataset,
+  generateMockPressureDataset,
+  generateMockStressRadialDataset,
+  generateMockTemperatureDataset,
+} from '../../../ui/chart/chart-data-mock';
+import { ChartDataset, ChartOptions } from '../../../ui/chart/chart.types';
 import SensorEdit from '../sensor-edit/sensor-edit';
 
 @Component({
   selector: 'app-table-header',
   imports: [MatButton, MatIcon, MatFormField, MatInput, TranslatePipe],
+  providers: [DatePipe],
   templateUrl: './table-header.html',
   styleUrl: './table-header.scss',
 })
 export class TableHeader {
   private readonly dialog = inject(MatDialog);
   protected readonly permissions = inject(PermissionsService);
+  private readonly datePipe = inject(DatePipe);
+  private readonly i18nService = inject(TranslateService);
 
   selectedSensorId = input<number | undefined>(undefined);
 
@@ -37,5 +49,48 @@ export class TableHeader {
 
   onDownload(): void {
     // Not implemented yet.
+  }
+
+  protected onPlot() {
+    const rangeFrom = this.datePipe.transform('2026-05-20');
+    const rangeTo = this.datePipe.transform('2026-06-24');
+    const datasets: ChartDataset[] = [
+      generateMockPressureDataset(),
+      generateMockStressRadialDataset(),
+      generateMockTemperatureDataset(),
+      generateMockHumidityDataset(),
+    ];
+    const title = this.i18nService.translate('chart.title', {
+      name: 'MyFancyExperiment',
+      rangeFrom: rangeFrom,
+      rangeTo: rangeTo,
+    })();
+    const options: ChartOptions = {
+      title: title,
+      subtitle: 'Plot of 666 measurements',
+      xAxisType: 'time',
+      xAxisLabel: 'Date',
+      yAxisLabels: {
+        y: 'Fluid Pressure [kPa]',
+        y2: 'Radial Stress [bar]',
+        y3: 'Temperature [°C]',
+        y4: 'Relative Humidity [%]',
+      },
+    };
+
+    this.dialog.open(Chart, {
+      width: '95vw',
+      maxWidth: '100%',
+      height: '95vh',
+      maxHeight: '100%',
+      autoFocus: true,
+      bindings: [
+        inputBinding('title', () => title),
+        inputBinding('datasets', () => datasets),
+        inputBinding('options', () => options),
+        outputBinding('pointClick', (event) => console.log('click: ', event)),
+        outputBinding('pointHover', (event) => console.log('hover: ', event)),
+      ],
+    });
   }
 }
