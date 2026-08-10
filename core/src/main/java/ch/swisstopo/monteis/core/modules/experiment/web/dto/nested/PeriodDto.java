e33b59955fe082e870248a75b579c28c26be76fd
Miguel Lehmann <lehmann@puzzle.ch>
2026-08-10 16:08:05 +0200

    fix(MON-122): use mat close action

--- core/src/main/java/ch/swisstopo/monteis/core/modules/experiment/web/dto/nested/PeriodDto.java
+++ core/src/main/java/ch/swisstopo/monteis/core/modules/experiment/web/dto/nested/PeriodDto.java
@@ -3,5 +3,4 @@ package ch.swisstopo.monteis.core.modules.experiment.web.dto.nested;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 
-public record PeriodDto(
-    @NotNull LocalDate start, @NotNull LocalDate end) {}
+public record PeriodDto(@NotNull LocalDate start, @NotNull LocalDate end) {}
