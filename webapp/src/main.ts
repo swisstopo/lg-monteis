0dc9db912cbbb9447547f34a2ce79057361816d6
Yanick Minder <minder@puzzle.ch>
2026-09-03 13:59:39 +0200

    fix(MON-146): fix error ag-grid #309 by providing undefined as default value for the rows input property in Table  #146

--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -1,7 +1,11 @@
+import { isDevMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
+import { AllCommunityModule, enableDevValidations, ModuleRegistry } from 'ag-grid-community';
 import { App } from './app/app';
 import { appConfig } from './app/app.config';
 
+if (isDevMode()) {
+  enableDevValidations();
+}
 ModuleRegistry.registerModules([AllCommunityModule]);
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
