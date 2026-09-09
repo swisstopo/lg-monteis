aa4d9a2854dc6800fceffb2356c9674776cbbecf
Yanick Minder <minder@puzzle.ch>
2026-09-09 14:20:37 +0200

    feat(MON-142): add fulcrum service and use it in sensor service  #142

--- core/src/main/java/ch/swisstopo/monteis/core/modules/sensor/service/SensorService.java
+++ core/src/main/java/ch/swisstopo/monteis/core/modules/sensor/service/SensorService.java
@@ -1,6 +1,8 @@
 package ch.swisstopo.monteis.core.modules.sensor.service;
 
 import ch.swisstopo.monteis.core.infrastructure.exception.ObjectBusinessValidationException;
+import ch.swisstopo.monteis.core.infrastructure.fulcrum.FulcrumSensor;
+import ch.swisstopo.monteis.core.infrastructure.fulcrum.FulcrumService;
 import ch.swisstopo.monteis.core.infrastructure.javers.AuditChanges;
 import ch.swisstopo.monteis.core.infrastructure.kafka.SensorConfigPublisher;
 import ch.swisstopo.monteis.core.modules.sensor.domain.Formula;
@@ -10,6 +12,7 @@ import ch.swisstopo.monteis.core.modules.sensor.domain.SensorRepository;
 import ch.swisstopo.monteis.core.modules.sensor.domain.SensorType;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
@@ -18,14 +21,24 @@ import org.springframework.stereotype.Service;
 public class SensorService {
   private final SensorRepository repository;
   private final SensorConfigPublisher configPublisher;
+  private final FulcrumService fulcrumService;
 
-  public SensorService(SensorRepository repository, SensorConfigPublisher configPublisher) {
+  public SensorService(
+      SensorRepository repository,
+      SensorConfigPublisher configPublisher,
+      FulcrumService fulcrumService) {
     this.repository = repository;
     this.configPublisher = configPublisher;
+    this.fulcrumService = fulcrumService;
   }
 
   @AuditChanges
   public Sensor createSensor(Sensor sensor) {
+    Optional<FulcrumSensor> sensorById = fulcrumService.getSensorById(sensor.getFulcrumId());
+    if (sensorById.isEmpty()) {
+      throw new ObjectBusinessValidationException("object.deleted", Map.of());
+    }
+
     Sensor created = repository.create(sensor);
     for (SensorParameter parameter : created.getParameters()) {
       configPublisher.publish(created, parameter);
@@ -36,6 +49,11 @@ public class SensorService {
   @AuditChanges
   public Sensor updateSensor(Sensor sensor) {
     Sensor before = repository.findById(sensor.getId()).orElse(null);
+
+    Optional<FulcrumSensor> sensorById = fulcrumService.getSensorById(sensor.getFulcrumId());
+    if (sensorById.isEmpty()) {
+      throw new ObjectBusinessValidationException("object.deleted", Map.of());
+    }
     Sensor updated = repository.update(sensor);
 
     Map<UUID, SensorParameter> parametersBefore =
