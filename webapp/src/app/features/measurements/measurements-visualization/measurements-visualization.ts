4f6a54b55df0ad9059b9309998979639c45f02d7
Augustin Trancart <augustin.trancart@oslandia.com>
2026-09-10 19:19:57 +0200

    feat(MON-151): display all the sensors in the scene

--- webapp/src/app/features/measurements/measurements-visualization/measurements-visualization.ts
+++ webapp/src/app/features/measurements/measurements-visualization/measurements-visualization.ts
@@ -1,12 +1,15 @@
-import { Component, effect, inject } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { Component, effect, inject, signal } from '@angular/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 import { WorkbenchView } from '@scion/workbench';
 import { Giro3d } from '../../../ui/giro3d/giro3d';
 import { TilesFetch } from '../../../ui/giro3d/tiles-fetch-plugin';
 import { MeasurementsTiles, TILESET_URL_PREFIX } from '../services/measurements-tiles';
 
+import { SensorControllerService, SensorResponseDto } from '../../../core/generated';
+import { InlineError } from '../../../ui/inline-error/inline-error';
+
 @Component({
-  imports: [Giro3d],
+  imports: [Giro3d, InlineError, TranslatePipe],
   selector: 'app-measurements-visualization',
   styleUrl: './measurements-visualization.scss',
   templateUrl: './measurements-visualization.html',
@@ -18,10 +21,36 @@ export default class MeasurementsVisualization {
 
   protected readonly tilesetUrl = `${TILESET_URL_PREFIX}/monteis-octree-poc/tileset.json`;
   protected readonly fetchTiles: TilesFetch = (url, options) => this.tiles.fetch(url, options);
+  private readonly sensorControllerService = inject(SensorControllerService);
+
+  protected readonly error = signal(false);
+  protected sensors = signal<SensorResponseDto[]>([]);
 
   constructor() {
     effect(() => {
       this.view.title = this.translate.translate('tab.measurements-visualization')();
     });
+  }
+
+  ngOnInit() {
+    const response = this.sensorControllerService.getSensors(0, 100);
+
+    let sensors = [];
+    response
+      .forEach((results) => {
+        if (results.rows) {
+          for (let sensor of results.rows) {
+            if (sensor.coordinates == null) {
+              console.log(`Sensor ${sensor.name} has no coordinates, skipping.`);
+            }
+            sensors.push(sensor);
+            this.sensors.set([...sensors]);
+          }
+        }
+      })
+      .catch((error) => {
+        console.error('Sensor fetching issue', error);
+        this.error.set(true);
+      });
   }
 }
