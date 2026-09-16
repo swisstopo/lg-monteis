0f19413fa51645d6488af140bc3cf0ff9edc6654
Paco Eggimann <peggimann@puzzle.ch>
2026-09-16 14:39:53 +0200

    fix(deps): add comment to timescale db in order to deploy db migrations

--- db/timescale/schema/V1__init.sql
+++ db/timescale/schema/V1__init.sql
@@ -3,6 +3,7 @@
 CREATE TYPE range_category AS ENUM ('too_low', 'correct', 'too_high');
 
 -- 2. Base Table erstellen (Raw Data)
+-- Hinweis: sensor_id ist sensor_code in der Metadaten DB
 CREATE TABLE IF NOT EXISTS sensor_reading (
                                               timestamp   TIMESTAMPTZ NOT NULL,
                                               sensor_id   TEXT NOT NULL,
