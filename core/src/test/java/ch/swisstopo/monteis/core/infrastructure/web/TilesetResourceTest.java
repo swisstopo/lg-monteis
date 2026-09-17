72e45ab042c50b77d18ab7e8b0c558367fad4b20
Augustin Trancart <augustin.trancart@oslandia.com>
2026-09-17 12:30:56 +0200

    feat(MON-150): fix the dataset manually
    In commit 6f85204368e3cae44a7fc640120704d96ec1e9f6, I've temporarily
    changed the dataset so that the bounding boxes were placed correcty.
    
    This commit uses the previous dataset after manually fixing the
    orientation of the mockup. The problem was a discrepancy between the
    gltf and GIS convention of UP axis.
    
    For the record, here is the python script I used:
    
    ```python
    from pathlib import Path
    
    from py3dtiles.tileset import TileSet
    from py3dtiles.constants import Z_UP_TO_Y_UP
    from py3dtiles.tileset.tile import Tile
    from py3dtiles.tileset.content.gltf import Gltf
    
    TILESET_PATH = Path(
        "/home/augustin/workspace/swisstopo/lg-monteis/core/src/main/resources/static/api/tilesets/monteis-octree-poc/tileset.json"
    )
    
    tileset = TileSet.from_file(TILESET_PATH)
    
    def rotate_and_recurse(tileset: TileSet, tile: Tile) -> None:
        if tile.has_content():
            tile_content = tile.get_or_fetch_content(tileset.root_uri)
            assert isinstance(tile_content, Gltf)
            for node in tile_content._gltf.nodes:
                assert node.matrix is None
                node.matrix = Z_UP_TO_Y_UP.flatten("F").tolist()
        for child in tile.children:
            rotate_and_recurse(tileset, child)
    
    rotate_and_recurse(tileset, tileset.root_tile)
    tileset.write_to_directory(TILESET_PATH, overwrite=True)
    ```

--- core/src/test/java/ch/swisstopo/monteis/core/infrastructure/web/TilesetResourceTest.java
+++ core/src/test/java/ch/swisstopo/monteis/core/infrastructure/web/TilesetResourceTest.java
@@ -24,7 +24,7 @@ import org.springframework.test.web.servlet.MockMvc;
 class TilesetResourceTest {
 
   private static final String TILESET = "/api/tilesets/monteis-octree-poc/tileset.json";
-  private static final String TILE = "/api/tilesets/monteis-octree-poc/geometry/1.b3dm";
+  private static final String TILE = "/api/tilesets/monteis-octree-poc/tile_13.glb";
 
   @Autowired private MockMvc mockMvc;
 
